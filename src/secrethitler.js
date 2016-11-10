9a6745b7a3869559bf0f1317031b023ebe9d43d3
Steven Vergenz <vergenzs@gmail.com>
2016-11-10 00:27:27 -0800

    Changed SH to be its own root obj

--- src/secrethitler.js
+++ src/secrethitler.js
@@ -3,15 +3,19 @@
 import * as Cards from './card';
 import AssetManager from './assets';
 
-export default //SecretHitler
+class SecretHitler extends THREE.Object3D
 {
-	assets: AssetManager.manifest,
-	verticalAlign: 'bottom',
-	initialize: (env, root, assets) => {
+	constructor()
+	{
+		super();
+		this.assets = AssetManager.manifest;
+		this.verticalAlign = 'bottom';
+	}
 
+	initialize(env, root, assets)
+	{
 		// populate the shared asset cache
 		AssetManager.cache = assets;
-		AssetManager.root = root;
 
 		// create the table
 		let table = assets.models.board;
@@ -19,22 +23,32 @@ export default //SecretHitler
 		table.children[0].material = mat;
 		table.rotation.set(-Math.PI/2, 0, 0);
 		table.position.set(0, 0.75, 0);
-		root.add(table);
+		this.add(table);
 
 		// create test card
-		let c = new Cards.FascistPolicyCard();
+		let c = new Cards.HitlerRoleCard();
 		c.translateY(1.3);
-		root.add(c);
+		this.add(c);
 
+		// create president hat
 		let hat = assets.models.tophat;
 		hat.position.set(0.5, 1.3, 0);
 		hat.rotation.set(-Math.PI/2, 0, 0);
-		root.add(hat);
+		this.add(hat);
 		
 		let hat2 = assets.models.visorcap;
 		hat2.position.set(-0.5, 1.3, 0);
 		hat2.rotation.set(-Math.PI/2, 0, 0);
-		root.add(hat2);
+		this.add(hat2);
+
+		// connect to server
+	}
+
+	// pass along client events to the server
+	notifyServer(evt)
+	{
+
 	}
 }
 
+export default new SecretHitler();
