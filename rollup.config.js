b989b1fc9c080d2a040d36fac1f19b722ee380ae
Steven Vergenz <vergenzs@gmail.com>
2016-11-13 12:43:07 -0800

    Reorg js sources

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import buble from 'rollup-plugin-buble';
 
 export default {
-	entry: 'src/secrethitler.js',
+	entry: 'src/client/secrethitler.js',
 	dest: 'static/js/secrethitler.js',
 	format: 'iife',
 	moduleName: 'SecretHitler',
