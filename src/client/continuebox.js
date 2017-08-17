e1ed346887d33d2fce6eb743faa8f195d6183c72
Steven Vergenz <vergenzs@gmail.com>
2017-08-16 23:12:56 -0700

    Fixed native components

--- src/client/continuebox.js
+++ src/client/continuebox.js
@@ -11,17 +11,24 @@ export default class ContinueBox extends THREE.Object3D
 	{
 		super();
 		this.icon = new THREE.Mesh(
-			new THREE.BoxBufferGeometry(.1, .1, .1),
+			new THREE.BoxBufferGeometry(.2, .1, .2),
 			new THREE.MeshBasicMaterial({color: 0x00c000})
 		);
 		this.icon.addBehavior(new altspace.utilities.behaviors.Spin());
 		this.add(this.icon);
 
 		this.text = new THREE.Mesh(placeholderGeo, placeholderMat);
+		this.text.position.set(0, .2, 0);
+		this.text.material.visible = false;
+		this.text.userData.altspace = {collider: {enabled: false}};
+
 		altspace.addNativeComponent(this.text, 'n-billboard');
-		altspace.updateNativeComponent(this.text, 'n-billboard', {});
+
+		// must send all properties every update, or they'll zero out
+		this.textData = {fontSize: 1, text: 'Test', width: 1, height: 1, horizontalAlign: 'middle', verticalAlign: 'middle'};
 		altspace.addNativeComponent(this.text, 'n-text');
-		altspace.updateNativeComponent(this.text, 'n-text', {fontSize: 1});
+		altspace.updateNativeComponent(this.text, 'n-text', this.textData);
+
 		this.add(this.text);
 
 		this.position.set(0, 0.3, 0);
@@ -41,17 +48,19 @@ export default class ContinueBox extends THREE.Object3D
 	{
 		if(game.state === 'lameDuck'){
 			this.icon.visible = true;
-			this.text.visible = true;
-			altspace.updateNativeComponent(this.text, 'n-text', {text: 'Click to continue'});
+			//this.text.visible = true;
+			this.textData.text = 'Click to continue';
+			altspace.updateNativeComponent(this.text, 'n-text', this.textData);
 		}
 		else if(game.state === 'setup' && game.turnOrder.length >= 5){
 			this.icon.visible = true;
-			this.text.visible = true;
-			altspace.updateNativeComponent(this.text, 'n-text', {text: 'Click to start'});
+			//this.text.visible = true;
+			this.textData.text = 'Click to start';
+			altspace.updateNativeComponent(this.text, 'n-text', this.textData);
 		}
 		else {
 			this.icon.visible = false;
-			this.text.visible = false;
+			//this.text.visible = false;
 		}
 		
 	}
